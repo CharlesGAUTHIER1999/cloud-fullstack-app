6182ffeafa3c64dd499fd2a7e7eea389af36e91c
charles gauthier <charles.gauthier99@gmail.com>
2025-10-16 16:10:48 +0200

    ff

--- backend/database/factories/PostFactory.php
+++ backend/database/factories/PostFactory.php
@@ -2,10 +2,13 @@
 
 namespace Database\Factories;
 
+use App\Models\Post;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class PostFactory extends Factory
 {
+    protected $model = Post::class;
+
     public function definition(): array
     {
         return [
