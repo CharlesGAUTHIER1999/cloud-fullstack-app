85ea20e057fc1b4234f657c116afa2283b4f0605
charles gauthier <charles.gauthier99@gmail.com>
2025-10-17 11:31:24 +0200

    fix vercel 2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default ({ mode }) => {
-    const env = loadEnv(mode, process.cwd(), '')
+    const env = loadEnv(mode, process.cwd(), "");
 
     return defineConfig({
         plugins: [react()],
-        base: '/',
+        base: "./",
         server: {
             host: true,
             port: 5173,
@@ -15,7 +15,8 @@ export default ({ mode }) => {
             },
         },
         define: {
-            __APP_ENV__: env.APP_ENV,
+            "import.meta.env": env,
         },
-    })
-}
+    });
+};
+
