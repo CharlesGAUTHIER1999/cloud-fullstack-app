9507a5ea2384250694f9b757c8747a8a7e3f2813
CharlesGAUTHIER1999 <charles.gauthier99@gmail.com>
2026-03-20 09:39:22 +0100

    adding title

--- frontend/src/layouts/AppLayout.jsx
+++ frontend/src/layouts/AppLayout.jsx
@@ -19,7 +19,7 @@ export default function AppLayout() {
                     <div className="d-flex align-items-center gap-2">
                         {/* Logo */}
                         <Link className="navbar-brand fw-bold me-3" to="/">
-                            🚀 ApptestGauthier2
+                            🚀 ApptCloud
                         </Link>
 
                         {/* Posts Dropdown */}
